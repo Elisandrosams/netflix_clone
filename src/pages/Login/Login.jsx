039e8c6bf9cad0ae774e447b0671e1dc4498b20f
Elisandro Sams <elisandrosams@gmail.com>
2026-07-03 19:00:14 -0600

    added a try catch block to navigate from login page to home page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/Logo.png";
 import { login, signup } from "../../firebase";
-import netflix_spinner from "../../assets/netflix_spinner.png"
+import netflix_spinner from "../../assets/netflix_spinner.png";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In");
@@ -10,35 +11,67 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const user_auth= async (event) => {
+  const user_auth = async (event) => {
     event.preventDefault();
     setLoading(true);
-    if(signState==="Sign In"){
-      await login(email, password);
-    }else{
-      await signup(name, email, password);
+    try {
+      if (signState === "Sign In") {
+        await login(email, password);
+      } else {
+        await signup(name, email, password);
+      }
+      navigate("/");
+    } catch (error) {
+      console.error("Authentication failed:", error);
+      alert("Login/Signup failed. Please try again.");
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
-  }
+  };
 
-  return (
-    loading?<div className="login-spinner">
+  return loading ? (
+    <div className="login-spinner">
       <img src={netflix_spinner} alt="" />
-    </div>:
+    </div>
+  ) : (
     <div className="login">
       <img src={logo} alt="Logo" className="login-logo" />
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
           {signState === "Sign Up" ? (
-            <input value={name} onChange={(event)=>{setName(event.target.value)}} type="text" placeholder="Your Name" />
+            <input
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              type="text"
+              placeholder="Your Name"
+            />
           ) : (
             <></>
           )}
-          <input value={email} onChange={(event)=>{setEmail(event.target.value)}}type="email" placeholder="Email" />
-          <input value={password} onChange={(event)=>{setPassword(event.target.value)}}type="password" placeholder="Password" />
-          <button onClick={user_auth} type="submit">{signState}</button>
+          <input
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={user_auth} type="submit">
+            {signState}
+          </button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -50,12 +83,14 @@ const Login = () => {
         <div className="form-switch">
           {signState === "Sign In" ? (
             <p>
-              Don't have an account? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span>
+              Don't have an account?{" "}
+              <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span>
             </p>
           ) : (
-          <p>
-             Already have an account? <span onClick={() => setSignState("Sign In")}>Sign In</span>
-           </p>
+            <p>
+              Already have an account?{" "}
+              <span onClick={() => setSignState("Sign In")}>Sign In</span>
+            </p>
           )}
         </div>
       </div>
